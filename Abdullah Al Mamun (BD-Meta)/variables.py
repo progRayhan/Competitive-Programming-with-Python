1cdc9c55633774c382a7fce72428111cbeb1217f
Rayhan <prog.rayhan1@gmail.com>
2022-11-14 10:24:21 +0600

    hello world & variables added

---
+++ Abdullah Al Mamun (BD-Meta)/variables.py
@@ -0,0 +1,15 @@
+# variables
+
+# sum of two variables
+a = 10
+b = 12
+print(a+b)
+
+# print a string
+name = "Mohammad Rayhan"
+print(name)
+
+# print two variables
+name1 = "Hasanuzzaman Rafin"
+address  = 'vill:x, post:y, dist:z'
+print(name1, address)
